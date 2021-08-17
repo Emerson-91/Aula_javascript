dcbf7088ee8b3d175e803e2aee39d3ccbef80299
Emerson Henrique Correa dos Santos <65039657+Emerson-91@users.noreply.github.com>
2021-08-16 20:05:29 -0400

    estudo basico de Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let nome = "Emerson Henrique";
+let idade = 30;
+let idade2 = 10;
+let frase = "Japao teste de frase";
+//alert(nome + " tem "+idade+ " anos")
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japao", "Brasil").toUpperCase());
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
